dee6a648c2563de8e64dc8b63caab6ef5de69b90
Bobby <Bobby@KISSINGER>
2017-02-09 13:16:18 -0500

    Add project files.

---
+++ Meeedium/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Meeedium.MvcApplication" Language="C#" %>
---
+++ Meeedium/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Meeedium.Startup))]
+namespace Meeedium
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

c88a9b6350f968a37f9a9df259b5430692487815
Bobby <kissinger645@gmail.com>
2017-02-13 12:45:20 -0500

    Updated FB, twitter and GH links

---
+++ Meeedium/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Meeedium.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Meeedium.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            ApplicationUser userInstance = db.Users.Where(u => u.Id == userId).FirstOrDefault();
+            return View(userInstance);
+        }
+        [Route("User/{userName}")]
+        public ActionResult Detail(string userName)
+        {
+            ApplicationUser userInstance = db.Users.Where(u => u.UserName == userName).FirstOrDefault();
+            return View(userInstance);
+        }
+    }
+}
